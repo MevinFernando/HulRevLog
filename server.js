3bd6af7ec385f1562c1595e29cac79b8f8b326b3
MevinFernando <mfmevins@gmail.com>
2019-05-24 08:57:58 +0530

    added error handling and CORS support

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const retailers = require("./routes/api/retailers");
 const barcodes = require("./routes/api/barcodes");
@@ -8,15 +9,15 @@ const returns = require("./routes/api/returns");
 const pickups = require("./routes/api/pickups");
 const deliveryPersons = require("./routes/api/deliveryPersons");
 const salesPersons = require("./routes/api/salesPersons");
-
 const distributor = require("./routes/distributor");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(morgan("dev"));
 
 const db = require("./config/keys").mongoURI;
 
@@ -24,6 +25,18 @@ mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
+    return res.status(200).json({});
+  }
+});
 
 app.use("/api/retailers", retailers);
 app.use("/api/barcodes", barcodes);
@@ -33,6 +46,21 @@ app.use("/api/deliveryPersons", deliveryPersons);
 app.use("/api/salesPersons", salesPersons);
 
 app.use("/distributor", distributor);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE");
